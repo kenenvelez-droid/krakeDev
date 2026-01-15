27f0cd9c73ec9ed6221cfeac7aa7c69d96c997c0
kenenvelez@gmail.com <kenenvelez@gmail.com>
2026-01-14 20:52:19 -0500

    el boton modificar la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
-     console.log("ingresar a saludar ");
+    let cmpImagenSaludo; 
+    cmpImagenSaludo=document.getElementById("imgSaludo")
+    cmpImagenSaludo.src="./imagenes/200.gif"
+    console.log("ingresar a saludar ");
+
 }
