b8cd4e5405eaa0efd9d747821824e7f3dbe3f107
kenenvelez@gmail.com <kenenvelez@gmail.com>
2026-01-21 14:41:35 -0500

    ingresar datos completado

--- ejercicioGit/utilitarios/saludo.js
+++ ejercicioGit/utilitarios/saludo.js
@@ -1,10 +1,28 @@
-saludar=function(){
-//recuperar el valor de la una caja texto txtNombre
-let nombre=recuperarTexto("txtNombre")
-//recuperar el valor de la una caja texto txtNombre
-    let apellido=recuperarTexto("txtApellido")
+saludar = function () {
+    //recuperar el valor de la una caja texto txtNombre
+    let nombre = recuperarTexto("txtNombre")
+    //recuperar el valor de la una caja texto txtNombre
+    let apellido = recuperarTexto("txtApellido")
+    let edad = recuperarInt("txtEdad");
+
+    let estatura = recuperarFloat("txtEstatura");
 }
 recuperarTexto = function (idComponente) {
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
+    return valorIngresado;
+}
+
+recuperarInt = function (idComponente) {
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
+    return valorIngresado;
+}
+recuperarFloat = function (idComponente) {
     let componente;
     let valorIngresado;
     componente = document.getElementById(idComponente);
