cfaea817b6fb4c482bc070882e65a43f47e377f1
kenenvelez@gmail.com <kenenvelez@gmail.com>
2026-01-21 19:32:55 -0500

    calcular iva lista

--- puntoDeVenta.js
+++ puntoDeVenta.js
@@ -34,7 +34,7 @@ calcularValorTotal = function () {
         Si el caso de prueba es exitoso, hacer un commit
      */
     //6. Invocar a calcularValorDescuento y lo que devuelve guardar en la variable valorDescuento
-    valorDescuento=calcularValorDescuento(valorSubtotal,porcentajeDescuento)
+    valorDescuento = calcularValorDescuento(valorSubtotal, porcentajeDescuento)
     //7. Mostrar el resultado en el componente lblDescuento
     mostrarTexto("lblDescuento", valorDescuento)
     /*
@@ -47,7 +47,10 @@ calcularValorTotal = function () {
      */
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
     // El IVA debe calcularse sobre el valor del subtotal menos el descuento
+    valorIVA = calcularIva(valorSubtotal - valorDescuento)
     //9. Mostrar el resultado en el componente lblValorIVA    
+    mostrarTexto("lblValorIVA",valorIVA)
+
     /*
         Caso de prueba: 
             - cantidad: 10 
